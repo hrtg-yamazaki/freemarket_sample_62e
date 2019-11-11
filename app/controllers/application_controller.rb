5b6286a1fccb2f6836a39072e350797c8dae5955
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2019-11-11 12:05:28 +0900

    Basic認証の導入に関する設定(修正)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  before_action :basic_auth if: :production?
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   private
