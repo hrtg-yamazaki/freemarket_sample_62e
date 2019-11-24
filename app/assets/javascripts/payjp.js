f399d51429955e7d09c1ecdf16bd6d97a64bb7b1
takao8989 <56573708+takao8989@users.noreply.github.com>
2019-11-24 19:24:20 +0900

    クレジットカード登録機能　修正１

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -21,7 +21,6 @@ document.addEventListener(
               $('<input type="hidden" name="payjp-token">').val(response.id)
             );
             document.inputForm.submit();
-            alert("登録が完了しました");
           } else {
             alert("カード情報が正しくありません。");
           }
