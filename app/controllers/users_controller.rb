f3ae81737e31e05b894e2764798c9ac7e8859ec3
takao8989 <56573708+takao8989@users.noreply.github.com>
2019-11-27 11:08:32 +0900

    クレジットカード情報　表示・編集
    マイページからのクレジットカード情報の表示と削除機能を追加しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,5 +20,61 @@ class UsersController < ApplicationController
 
   end
 
-end
+  ## 登録カード情報表示・編集 ##
+  require "payjp"
 
+  def card
+    card = Card.where(user_id: current_user.id).first
+    # if card.blank?
+    #   redirect_to action: "card"
+    # else
+    #   Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+    #   customer = Payjp::Customer.retrieve(card.pay_id)
+    #   @default_card_information = customer.cards.retrieve(card.card_id)
+    # end
+
+    unless card.blank?
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      customer = Payjp::Customer.retrieve(card.pay_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+  def card_create
+
+  end
+
+  def card_update
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+    if params['payjp-token'].blank?
+      redirect_to action: "card"
+    else
+      customer = Payjp::Customer.create(
+        card: params['payjp-token'],
+        )
+        @card = Card.new(user_id: current_user.id, pay_id: customer.id, card_id: customer.default_card)
+        if @card.save
+          redirect_to action: "card"
+        else
+          redirect_to action: "card_update"
+        end
+      end
+  end
+
+  def complete
+
+  end
+
+  def card_delete
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else 
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      customer = Payjp::Customer.retrieve(card.pay_id)
+      customer.delete
+      card.delete
+    end
+    redirect_to action: "card"
+  end
+
+end
